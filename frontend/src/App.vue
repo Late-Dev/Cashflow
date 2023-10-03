d2c545a2ad23540f895ada3d01d7abf3b590f3bc
Ivan Riazanov <omega19961@gmail.com>
2023-10-03 22:10:13 +0600

    added dohnut chart and chart js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,37 @@
 
 <template>
-  <div>
-    <button>Add new</button>
+  <div class="app">
+    <div class="app_navbar">
+
+    </div>
+    <div class="card app__card">
+
+
+      <CategoryChart />
+      <button class="app__new-btn">Add new</button>
+    </div>
   </div>
 </template>
 
 
 <script setup lang="ts">
+import CategoryChart from './components/CategoryChart/CategoryChart.vue'
+
+
 </script>
 
 <style scoped lang="scss">
+.app{
+
+  &__card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  &__new-btn{
+    margin-top: 1em;
+  }
+}
+
 </style>
---
+++ frontend/src/components/CategoryChart/CategoryChart.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Doughnut :data="data"></Doughnut>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { ref } from 'vue';
+import { Doughnut } from 'vue-chartjs'
+import {
+    Chart as ChartJS,
+    ArcElement,
+    ChartData,
+    Colors
+} from 'chart.js'
+
+
+
+ChartJS.register(ArcElement, Colors)
+
+const data = ref<ChartData<"doughnut">>({ datasets: [{ data: [1, 2, 3, 4, 5] }] })
+
+</script>
+
+<style scoped lang='scss'></style>
