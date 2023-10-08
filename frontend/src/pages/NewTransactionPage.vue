3295eff6d88a0643e19c503c8d54f7f67e6be9d0
Ivan Riazanov <omega19961@gmail.com>
2023-10-08 18:33:07 +0600

    new transaction inputs

--- frontend/src/pages/NewTransactionPage.vue
+++ frontend/src/pages/NewTransactionPage.vue
@@ -3,10 +3,32 @@
     <div class="q-ma-sm">
       <ModeToggle />
     </div>
-    <div>
+    <div class="q-pa-sm">
 
-      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-        <q-input filled v-model="text" label="Filled" />
+      <q-form @submit="onSubmit" @reset="onReset">
+        <div class="new-transaction__group">
+
+          <q-input dense filled square outlined bg-color="secondary" label-color="dark" color="dark" v-model="text"
+            label="The amount" />
+          <q-input dense filled square outlined bg-color="secondary" label-color="dark" color="dark" v-model="text"
+            label="Currency" />
+        </div>
+
+        <div class="new-transaction__group">
+
+          <q-input dense filled square outlined bg-color="secondary" label-color="dark" color="dark" v-model="text"
+            label="Category" />
+          <q-input dense filled square outlined bg-color="secondary" label-color="dark" color="dark" v-model="text"
+            label="Source" />
+          <q-input dense filled square outlined bg-color="secondary" label-color="dark" color="dark" v-model="text"
+            label="Comment" />
+        </div>
+
+        <div class="new-transaction__group">
+
+          <q-input dense filled square borderless bg-color="secondary" label-color="dark" color="dark" v-model="text"
+            label="Date" />
+        </div>
       </q-form>
     </div>
   </q-page>
@@ -18,4 +40,32 @@ import ModeToggle from 'src/components/ModeToggle.vue';
 
 </script>
 
-<style scoped lang='scss'></style>
+<style scoped lang='scss'>
+.new-transaction {
+  &__group {
+    margin-bottom: 14px;
+
+    border-radius: 12px;
+    overflow: hidden;
+
+    :deep(input) {
+      color: $theme-primary-text;
+    }
+
+    :deep(.q-field__label) {
+      opacity: 0.7;
+    }
+
+    :deep(.q-field__control::after) {
+
+      display: none;
+
+    }
+
+    :deep(.q-field__control::before) {
+      border: none;
+    }
+
+  }
+}
+</style>
